aee39bd04da57e2613c534250ceb6c1572e64d28
jackdeye <jackdeye@ucla.edu>
2025-03-13 11:17:01 -0700

    Organized Files

--- Quadratures/gauss_legendre_quadrature.m
+++ Quadratures/gauss_legendre_quadrature.m
@@ -3,5 +3,6 @@ function I = gauss_legendre_quadrature(f, a, b, n)
     % Can also use naive_nodes_weights, but it is much slower
  [nodes, weights] = GW_nodes_weights(n);
  transformed_nodes = ((b - a) / 2) * nodes + (a + b) / 2;
- I = ((b - a) / 2) * sum(weights .* f(transformed_nodes));
+ values = f(transformed_nodes);
+ I = ((b - a) / 2) * sum(weights .* values);
 end
